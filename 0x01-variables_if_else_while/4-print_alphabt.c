28e254cec73bc681cf6c149ae8ae7a3ba5100fe5
DrSimaB <calvinbenatha@gmail.com>
2022-09-09 14:53:27 -0800

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,11 +14,10 @@ int main(void)
 	q = 'q';
 
 	for (low = 'a'; low <= 'z'; low++)
-	{
-		if (low != e && low != q)
-			putchar(low);
-	}
+{
+	if (low != e && low != q)
+		putchar(low);
+}
 		printf("\n");
-
 	return (0);
 }
